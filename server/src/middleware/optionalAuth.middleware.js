281ad89f52e8b166adb071c490e5343e96f568ca
Sanketh Nayak <160486607+Sanketh-Nayak@users.noreply.github.com>
2026-07-07 10:35:53 +0530

    Configaration

--- server/src/middleware/optionalAuth.middleware.js
+++ server/src/middleware/optionalAuth.middleware.js
@@ -1,13 +1,14 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
 
 const optionalAuth = async (req, res, next) => {
   try {
     const header = req.headers.authorization;
-    if (header?.startsWith('Bearer ')) {
-      const token = header.split(' ')[1];
+    if (header?.startsWith("Bearer ")) {
+      const token = header.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select('-password');
+      req.user = await User.findById(decoded.id).select("-password");
     }
   } catch {
     // ignore invalid token for public routes
